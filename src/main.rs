b5084f7e637e501c0bd8202ab2baa630c58f307b
Toby Hutton <toby@grusly.com>
2021-07-21 00:48:40 +1000

    Add support for compiling `if` statements.

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,8 @@ fn main() -> Result<(), std::io::Error> {
     src_file.read_to_string(&mut input_string)?;
 
     // Parse into an AST.
-    let ast = parser::parse_string(&input_string)?;
-    //println!("{:?}", ast);
+    let program = parser::parse_string(&input_string)?;
+    //println!("{:?}", program);
     //return Ok(());
 
     // Create a JIT module.
@@ -46,12 +46,36 @@ fn main() -> Result<(), std::io::Error> {
     let mut fn_ctx = FunctionBuilderContext::new();
     let mut fn_builder = FunctionBuilder::new(&mut ctx.func, &mut fn_ctx);
 
-    // Translate our AST into IR.
-    compile(&mut module, &mut fn_builder, ast);
+    // Gather and declare all our immediate strings.
+    let mut data_map = HashMap::<Vec<u8>, DataId>::new();
+    let mut str_id = 0_usize;
+    compile_data(&mut module, &mut data_map, &mut str_id, &program);
+    module.finalize_definitions();
+
+    // Create the entry block.
+    let block = fn_builder.create_block();
+    fn_builder.switch_to_block(block);
+    fn_builder.seal_block(block);
+
+    // Declare all variables in this block.
+    let mut var_map = HashMap::<String, Variable>::new();
+    let mut var_id = 0_usize;
+    declare_all_variables(&mut fn_builder, &mut var_map, &mut var_id, &program);
+
+    // Compile the program.
+    let mut compiler = Compiler {
+        module: &mut module,
+        fn_builder: fn_builder,
+        data_map: data_map,
+        var_map: var_map,
+    };
+    compiler.compile_code(&program);
 
     // Finalize the main function.
-    fn_builder.seal_all_blocks();
-    fn_builder.finalize();
+    compiler.fn_builder.ins().return_(&[]);
+    compiler.fn_builder.finalize();
+    compiler.fn_builder.seal_all_blocks();
+    compiler.fn_builder.finalize();
     let mut trap_sink = codegen::binemit::NullTrapSink {};
     let mut stack_map_sink = codegen::binemit::NullStackMapSink {};
     module
@@ -77,30 +101,6 @@ where
     E: Into<Box<dyn std::error::Error + Send + Sync>>,
 {
     std::io::Error::new(std::io::ErrorKind::Other, err)
-}
-
-// -------------------------------------------------------------------------------------------------
-
-fn compile(module: &mut JITModule, fn_builder: &mut FunctionBuilder, program: AstNode) {
-    // Gather and declare all our immediate strings.
-    let mut data_map = HashMap::<Vec<u8>, DataId>::new();
-    let mut str_id = 0_usize;
-    compile_data(module, &mut data_map, &mut str_id, &program);
-    module.finalize_definitions();
-
-    // Create the entry block.
-    let block = fn_builder.create_block();
-    fn_builder.switch_to_block(block);
-    fn_builder.seal_block(block);
-
-    // Declare all variables in this block.
-    let mut var_map = HashMap::<String, Variable>::new();
-    let mut var_id = 0_usize;
-    declare_all_variables(fn_builder, &mut var_map, &mut var_id, &program);
-
-    // Compile the program.
-    compile_code(module, fn_builder, &data_map, &var_map, &program);
-    fn_builder.finalize();
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -213,54 +213,176 @@ fn declare_variable(
 
 // -------------------------------------------------------------------------------------------------
 
-fn compile_code(
-    module: &mut JITModule,
-    fn_builder: &mut FunctionBuilder,
-    data_map: &HashMap<Vec<u8>, DataId>,
-    _var_map: &HashMap<String, Variable>,
-    program: &AstNode,
-) {
-    match program {
-        AstNode::Call(name, args) => {
-            // At the moment, for this demo, the only function we do call is `print` and it takes a
-            // single literal argument.
-            assert!(name == "print");
-            assert!(args.len() == 1);
-            match &args[0] {
-                AstNode::Literal(AstValue::Text(s)) => {
-                    compile_print_str(module, fn_builder, data_map, s)
-                }
-                _ => panic!("Only strings are supported so far..."),
-            }
-        }
-        _ => panic!("unhandled node: {:?}", program),
-    }
-    fn_builder.ins().return_(&[]);
+struct Compiler<'a> {
+    module: &'a mut JITModule,
+    fn_builder: FunctionBuilder<'a>,
+    data_map: HashMap<Vec<u8>, DataId>,
+    var_map: HashMap<String, Variable>,
 }
 
-fn compile_print_str(
-    module: &mut JITModule,
-    fn_builder: &mut FunctionBuilder,
-    data_map: &HashMap<Vec<u8>, DataId>,
-    str_val: &Vec<u8>,
-) {
-    // int puts(const char* str)
-    let mut sig = module.make_signature();
-    let ptr_type = module.target_config().pointer_type();
-    sig.params
-        .push(AbiParam::new(ptr_type));
-    sig.returns.push(AbiParam::new(types::I32));
+impl<'a> Compiler<'a> {
+    fn compile_code(&mut self, program: &AstNode) -> Value {
+        match program {
+            AstNode::Literal(AstValue::Int(i)) => self.fn_builder.ins().iconst(types::I64, *i),
 
-    let libc_puts = module
-        .declare_function("puts", Linkage::Import, &sig)
-        .expect("Failed to declare `puts()`");
-    let callee = module.declare_func_in_func(libc_puts, &mut fn_builder.func);
+            AstNode::Call(name, args) => {
+                // At the moment, for this demo, the only function we do call is `print` and it takes a
+                // single literal or an identifier referencing an int value.
+                assert!(name == "print");
+                assert!(args.len() == 1);
+                match &args[0] {
+                    AstNode::Literal(AstValue::Text(s)) => self.compile_print_str(s),
+                    AstNode::Literal(AstValue::Int(i)) => self.compile_print_int(*i),
+                    AstNode::Identifier(i) => self.compile_print_sym(i),
 
-    let data_id = data_map.get(str_val).unwrap();
-    let local_id = module.declare_data_in_func(*data_id, &mut fn_builder.func);
+                    _ => panic!("unexpected argument for print()!"),
+                }
+            }
 
-    let arg = fn_builder.ins().symbol_value(ptr_type, local_id);
-    fn_builder.ins().call(callee, &vec![arg]);
+            AstNode::If {
+                cond_expr,
+                true_expr,
+                false_expr,
+            } => self.compile_if(cond_expr, true_expr, false_expr),
+
+            _ => panic!("unhandled node: {:?}", program),
+        }
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    fn compile_if(
+        &mut self,
+        cond_expr: &AstNode,
+        true_exprs: &[AstNode],
+        false_exprs: &[AstNode],
+    ) -> Value {
+        let cond_val = self.compile_code(cond_expr);
+
+        let true_block = self.fn_builder.create_block();
+        let false_block = self.fn_builder.create_block();
+        let final_block = self.fn_builder.create_block();
+
+        // Jump to block depending on condition.
+        self.fn_builder.ins().brz(cond_val, false_block, &[]);
+        self.fn_builder.ins().jump(true_block, &[]);
+
+        // Populate the true block, jump to final block at end.
+        self.fn_builder.switch_to_block(true_block);
+        self.fn_builder.seal_block(true_block);
+        for expr in true_exprs {
+            self.compile_code(expr);
+        }
+        self.fn_builder.ins().jump(final_block, &[]);
+
+        // Populate the false block, jump to the final block at end.
+        self.fn_builder.switch_to_block(false_block);
+        self.fn_builder.seal_block(false_block);
+        for expr in false_exprs {
+            self.compile_code(expr);
+        }
+        self.fn_builder.ins().jump(final_block, &[]);
+
+        // Switch to final block for rest of program.
+        self.fn_builder.switch_to_block(final_block);
+        self.fn_builder.seal_block(final_block);
+
+        // Need to return a dummy null value.
+        self.fn_builder.ins().iconst(types::I64, 0)
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    fn compile_print_str(&mut self, str_val: &Vec<u8>) -> Value {
+        // int puts(const char* str)
+        let mut sig = self.module.make_signature();
+        let ptr_type = self.module.target_config().pointer_type();
+        sig.params.push(AbiParam::new(ptr_type));
+        sig.returns.push(AbiParam::new(types::I32));
+
+        let libc_puts = self
+            .module
+            .declare_function("puts", Linkage::Import, &sig)
+            .expect("Failed to declare `puts()`");
+        let callee = self
+            .module
+            .declare_func_in_func(libc_puts, &mut self.fn_builder.func);
+
+        let data_id = self.data_map.get(str_val).unwrap();
+        let local_id = self
+            .module
+            .declare_data_in_func(*data_id, &mut self.fn_builder.func);
+
+        let arg = self.fn_builder.ins().symbol_value(ptr_type, local_id);
+        self.fn_builder.ins().call(callee, &vec![arg]);
+        arg
+    }
+
+    fn compile_print_int(&mut self, int_val: i64) -> Value {
+        let value = self.fn_builder.ins().iconst(types::I32, int_val);
+        self.compile_print_int_value(value);
+        value
+    }
+
+    fn compile_print_sym(&mut self, ident: &str) -> Value {
+        let variable = self.var_map.get(ident).unwrap();
+        let value = self.fn_builder.use_var(*variable);
+        self.compile_print_int_value(value);
+        value
+    }
+
+    fn compile_print_int_value(&mut self, value: Value) {
+        // NOTE: This is a complete hack, because I want to get something working ASAP!
+        //
+        // We can only print positive integers, between 0 and 999.  To do that we print each digit in
+        // turn.
+
+        // int putchar(int c)
+        let mut sig = self.module.make_signature();
+        sig.params.push(AbiParam::new(types::I32));
+        sig.returns.push(AbiParam::new(types::I32));
+
+        let libc_putchar = self
+            .module
+            .declare_function("putchar", Linkage::Import, &sig)
+            .expect("Failed to declare `putchar()`");
+        let callee = self
+            .module
+            .declare_func_in_func(libc_putchar, &mut self.fn_builder.func);
+
+        let space = self.fn_builder.ins().iconst(types::I32, 32);
+
+        // First char, 100s column!
+        let var_div_100 = self.fn_builder.ins().udiv_imm(value, 100);
+        let var_0_mod_10 = self.fn_builder.ins().urem_imm(var_div_100, 10);
+        let var_0_digit = self.fn_builder.ins().iadd_imm(var_0_mod_10, 48); // digit + '0'
+        let var_0_is_z = self
+            .fn_builder
+            .ins()
+            .icmp_imm(IntCC::Equal, var_0_mod_10, 0);
+        let var_0_ch = self.fn_builder.ins().select(var_0_is_z, space, var_0_digit);
+        self.fn_builder.ins().call(callee, &vec![var_0_ch]);
+
+        // Second char, 10s column!
+        let var_div_10 = self.fn_builder.ins().udiv_imm(value, 10);
+        let var_1_mod_10 = self.fn_builder.ins().urem_imm(var_div_10, 10);
+        let var_1_digit = self.fn_builder.ins().iadd_imm(var_1_mod_10, 48); // digit + '0'
+        let var_1_is_z = self
+            .fn_builder
+            .ins()
+            .icmp_imm(IntCC::Equal, var_1_mod_10, 0);
+        let var_both_are_z = self.fn_builder.ins().band(var_0_is_z, var_1_is_z);
+        let var_1_ch = self
+            .fn_builder
+            .ins()
+            .select(var_both_are_z, space, var_1_digit);
+        self.fn_builder.ins().call(callee, &vec![var_1_ch]);
+
+        // Third char, 1s column!
+        let var_2_mod_10 = self.fn_builder.ins().urem_imm(value, 10);
+        let var_2_ch = self.fn_builder.ins().iadd_imm(var_2_mod_10, 48); // digit + '0'
+        self.fn_builder.ins().call(callee, &vec![var_2_ch]);
+    }
 }
 
 // -------------------------------------------------------------------------------------------------
